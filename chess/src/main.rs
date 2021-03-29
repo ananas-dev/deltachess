9d40053e1ae0960537f70cdcce1bfba36655210a
ananas-dev <lucienfiorini@protonmail.com>
2021-03-29 16:16:10 +0200

    refactor: :fire: clear client_ng

--- chess/src/main.rs
+++
@@ -1,7 +0,0 @@
-use chess::{Board, Game};
-
-fn main() {
-    let game = Game::new();
-    assert_eq!(game.current_position(), Board::default());
-    println!("{:?}", game.current_position().get_hash());
-}
